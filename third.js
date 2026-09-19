259d6c521c97289c1d95e5a74d269b9ce72648f4
Amit Patil <117425578+Asteriskkkk@users.noreply.github.com>
2026-09-19 10:34:58 +0530

    asdf

--- third.js
+++ third.js
@@ -1,48 +1,56 @@
 let TV = {
     name: "TV",
-    status: "OFF"
+    isTurnedOn: false
 };
 
 let Lamp = {
     name: "Lamp",
-    status: "OFF"
+    isTurnedOn: false
 };
 
 let Oven = {
     name: "Oven",
-    status: "OFF"
+    isTurnedOn: false
 };
+
 
 function turnOnAppliance(appliance) {
     try {
-        if (appliance.status === "ON") {
+        if (appliance.isTurnedOn === true) {
             throw new Error(appliance.name + " is already ON");
         }
 
-        appliance.status = "ON";
+        appliance.isTurnedOn = true;
         return appliance.name + " turned ON successfully";
+
     } catch (error) {
         return error.message;
+
     } finally {
         console.log("Operation completed");
     }
 }
+
 
 function turnOffAppliance(appliance) {
     try {
-        if (appliance.status === "OFF") {
+        if (appliance.isTurnedOn === false) {
             throw new Error(appliance.name + " is already OFF");
         }
 
-        appliance.status = "OFF";
+        appliance.isTurnedOn = false;
         return appliance.name + " turned OFF successfully";
+
     } catch (error) {
         return error.message;
+
     } finally {
         console.log("Operation completed");
     }
 }
 
+
+// Test
 console.log(turnOnAppliance(TV));
 console.log(turnOnAppliance(Lamp));
 console.log(turnOnAppliance(Oven));
